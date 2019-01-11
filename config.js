b03e1af2fe6aee1040693ad237c30051ecc27967
AxYleN <jeka2644@gmail.com>
2019-01-11 17:09:52 +0300

    Данные для конфигурации БД берутся из process.env переменной

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
   HOST: process.env.HOST || 'localhost',
-  DB_HOST: 'localhost',
-  DB_NAME: 'okna_plus',
-  DB_USER: 'root',
-  DB_PASS: '12345',
+  DB_HOST: process.env.DB_HOST || 'localhost',
+  DB_NAME: process.env.DB_NAME || 'okna_plus',
+  DB_USER: process.env.DB_USER || 'root',
+  DB_PASS: process.env.DB_PASS || '12345',
 };
 
 // При ошибке подключения к базе данных
