ed15469545feb0e9e503c9bd398bd0f6150fc59f
AxYleN <jeka2644@gmail.com>
2019-01-26 22:15:15 +0300

    API для работы с товарами

--- api/products.js
+++ api/products.js
@@ -2,15 +2,30 @@ const express = require('express');
 const routs = express.Router();
 
 const db = require('../lib/dbFunctions');
+const { mergeObjects } = require('../lib/functions');
 
 routs.get('/', async function(req, res) {
   if (req.query.extended) {
-    const productList = await db.getProductParams('w1', 'w2', 'w3');
+    const productList = await db.getProductParams();
     res.json(productList);
   } else {
     const productList = await db.getProductList();
     res.json(productList);
   }
+});
+
+routs.put('/', function(req, res) {
+  const keys = Object.keys(req.body);
+
+  db.getProductParams(...keys)
+    .then(async oldData => {
+      const newData = mergeObjects(oldData, req.body);
+      await db.setProductParams(newData);
+      res.json(newData);
+    })
+    .catch(err => {
+      res.status(404).send();
+    });
 });
 
 routs.get('/:id', async function(req, res) {
